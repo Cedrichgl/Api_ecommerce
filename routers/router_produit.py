09450f4a630035d5b866c32dc57af27b476ab738
Cedric <cedrichiheglo@gmail.com>
2026-04-20 19:49:46 +0200

    route users & produit

--- routers/router_produit.py
+++ routers/router_produit.py
@@ -1,9 +1,46 @@
 from fastapi import APIRouter, Depends, HTTPException
+from typing import List
 from sqlalchemy.orm import Session
 from database import get_db
-from schemas.schema_user import UserBase, UserCreate, UserResponse, UserUpdate
+from schemas.schema_produit import ProduitBase, ProduitCreate, ProduitResponse, ProduitUpdate
 from models import Produit
 
 router = APIRouter()
 
+
+@router.post("/produits/", response_model=ProduitResponse, status_code=201)
+async def create_produit(produit: ProduitCreate, db:Session = Depends(get_db)):
+    db_produit = Produit(**produit.model_dump())
+    db.add(db_produit)
+    db.commit()
+    db.refresh(db_produit)
+    return db_produit
+
+
+@router.get("/produits/", response_model=List[ProduitResponse])
+async def get_produits(db:Session = Depends(get_db)):
+    produits = db.query(Produit).all()
+    return produits
+
+
+@router.get("/produits/{produit_id}", response_model=ProduitResponse)
+async def get_produit(produit_id:int, db:Session = Depends(get_db)):
+    produit = db.query(Produit).filter(Produit.id == produit_id).first()
+    if not produit:
+        raise HTTPException(status_code=404, detail="Produit introuvable")
+    return produit
+
+
+@router.patch("/produits/{produit_id}", response_model=ProduitUpdate, status_code=200)
+async def patch_produit(produit_id:int, payload: ProduitUpdate, db:Session = Depends(get_db)):
+    produit = db.query(Produit).filter(Produit.id == produit_id).first()
+    if not produit:
+        raise HTTPException(status_code=404, detail="Produit introuvable")
+    for key, value in payload.model_dump(exclude_unset=True).items():
+        setattr(produit, key, value)
+
+    db.add(produit)
+    db.commit()
+    db.refresh(produit)
+    return produit
 
--- routers/router_user.py
+++ routers/router_user.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
+from typing import List
+from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from database import get_db
 from schemas.schema_user import UserBase, UserCreate, UserResponse, UserUpdate
@@ -7,9 +9,17 @@ from models import User
 router = APIRouter()
 
 #créer un utilisateur
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 @router.post("/users", response_model=UserResponse, status_code=201)
-async def create_user(user: UserCreate, db:Session = Depends(get_db)):
-    db_user = User(**user.model_dump())
+async def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    # convertir en dict
+    data = user.model_dump()
+    # retirer password
+    password = data.pop("password")
+    # hasher
+    hashed_password = pwd_context.hash(password[:72])
+    # créer user avec le bon champ
+    db_user = User(**data, password_hash=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
@@ -23,6 +33,15 @@ async def get_user(user_id: int, db: Session = Depends(get_db)):
     if not db_user:
         raise HTTPException(status_code=404, detail="Utilisateur introuvable")
     return db_user
+
+
+#obtenir tous les utilisateurs
+@router.get("/users/", response_model=List[UserResponse])
+async def get_all_users(skip:int = 0, limit:int = 0, db:Session = Depends(get_db)):
+    db_users = db.query(User).offset(skip).limit(limit).all()
+    return db_users
+
+
 
 #modification d'un utilisateur
 @router.patch("/users/{user_id}", response_model=UserResponse)
